e2e2b24e88feeaf35a563e4a47ff273785996768
lambert3 <lygobalambert@outlook.com>
2020-07-14 14:24:46 +0300

    added the data of the remaining quaters

--- mapGrids/views.py
+++ mapGrids/views.py
@@ -39,16 +39,19 @@ SELECT row_to_json(fc) FROM
             SELECT l FROM 
             (
             SELECT
-              agric_indicator.ppp_sum,agric_indicator.soil_copper,agric_indicator.soil_phos,agric_indicator.soil_potas,agric_indicator.soil_boron,
-              agric_indicator.soil_alumi,agric_indicator.soil_iron,soil_magne,agric_indicator.ndwi_jfm,agric_indicator.presp_jfm,
-              agric_indicator.ndvi_jfm,agric_indicator.land_cover,agric_indicator.slope,agric_indicator.elevation,agric_indicator.lst_jfm,
-              agric_indicator.gs_id
+              agric_indicator.ppp_sum,agric_indicator.soil_copper,agric_indicator.soil_phos,agric_indicator.soil_potas,
+              agric_indicator.soil_boron,agric_indicator.soil_alumi,agric_indicator.soil_iron,soil_magne,
+              agric_indicator.ndwi_jfm,agric_indicator.ndwi_amj,agric_indicator.ndwi_jas,agric_indicator.ndwi_ond,
+              agric_indicator.presp_jfm,agric_indicator.presp_amj,agric_indicator.presp_jas,agric_indicator.presp_ond,
+              agric_indicator.ndvi_jfm,agric_indicator.ndvi_amj,agric_indicator.ndvi_jas,agric_indicator.ndvi_ond,
+              agric_indicator.land_cover,agric_indicator.slope,agric_indicator.elevation,agric_indicator.lst_jfm,
+              agric_indicator.lst_amj,agric_indicator.lst_jas,agric_indicator.lst_ond,agric_indicator.gs_id
                 FROM public.agric_indicator
                 WHERE agric_indicator.gs_id= lg.gs_id
             ) As l
         )
         ) As properties , lg.id, lg.rsd_id, lg.gs_id
-        FROM public.geometric_structure  As lg
+        FROM public.geometric_structure  As lg where lg.gs_id<100
     ) As f 
 )  As fc;
  ''')
--- sliders/views.py
+++ sliders/views.py
@@ -21,8 +21,10 @@ def view(request):
     cursor=connection.cursor()
         # executing the query
 
-    cursor.execute('''select ppp_sum,soil_copper,soil_phos,soil_potas,soil_boron,soil_alumi,soil_iron,soil_magne,ndwi_jfm,presp_jfm,
-    ndvi_jfm,land_cover,slope,elevation,lst_jfm, gs_id from public.agric_indicator''')
+    cursor.execute('''select ppp_sum,soil_copper,soil_phos,soil_potas,soil_boron,soil_alumi,soil_iron,
+    soil_magne,ndwi_jfm,ndwi_amj,ndwi_jas,ndwi_ond,presp_jfm,presp_amj,presp_jas,presp_ond,ndvi_jfm,
+    ndvi_amj,ndvi_jas,ndvi_ond,land_cover,slope,elevation,lst_jfm,lst_amj,lst_jas,lst_ond,gs_id from 
+    public.agric_indicator where gs_id<100''')
 
 
 
