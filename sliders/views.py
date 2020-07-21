4afedeb622a5afeadabe8534c06b8615ec4853ca
RonKbS <stephen.bamwite@gmail.com>
2020-07-21 10:43:50 +0300

    pick more indicators from the database

--- sliders/views.py
+++ sliders/views.py
@@ -26,8 +26,10 @@ def view(request):
 # rainfall,soil_alumi,soil_potas,(lst_jfm+lst_amj+lst_jas+lst_ond)/4,slope,elevation,
 # land_cover, gs_id from public.agric_indicator where gs_id<10 ''')
 
-    cursor.execute('''select ppp_sum,soil_copper,soil_phos,soil_potas,soil_boron,soil_alumi,soil_iron,soil_magne,ndwi_jfm,presp_jfm,
-    ndvi_jfm,land_cover,slope,elevation,lst_jfm, gs_id from public.agric_indicator''')
+    cursor.execute('''select ppp_sum,soil_copper,soil_phos,soil_potas,soil_boron,soil_alumi,soil_iron,
+    soil_magne,ndwi_jfm,ndwi_amj,ndwi_jas,ndwi_ond,presp_jfm,presp_amj,presp_jas,presp_ond,ndvi_jfm,
+    ndvi_amj,ndvi_jas,ndvi_ond,land_cover,slope,elevation,lst_jfm,lst_amj,lst_jas,lst_ond,gs_id from 
+    public.agric_indicator''')
 
 
 
