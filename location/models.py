f2ea81fef589940540aa0c2a6bb467059b3879e6
lambert3 <lygobalambert@outlook.com>
2020-07-29 18:05:01 +0300

    structured location and sliders models

--- location/models.py
+++ location/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class regional_structure(models.Model):
+    region =models.CharField(max_length=50)
+    district =models.CharField(max_length=50)
+    rsd_id=models.IntegerField()
+
+    def save(self):
+        return super(regional_structure, self).save()
